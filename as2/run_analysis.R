c6d808c91102b8c6bd77757321f737cc0b44e703
rmartin84 <roberto.martin@hotmail.fr>
2015-04-17 15:58:31 +0200

    Update run_analysis.R

--- as2/run_analysis.R
+++ as2/run_analysis.R
@@ -1,32 +1,76 @@
-run_analysis <- function(directory = "C:\\Users\\roberto.martin\\Downloads\\rprog_data_ProgAssignment3-data\\cleaning\\UCI_HAR_Dataset") {
+run_analysis <- function(directory = "C:\\Users\\Downloads\\rprog_data_ProgAssignment3-data\\cleaning\\UCI_HAR_Dataset") {
   
-setwd(directory)
+  setwd(directory)
 
+  # set up activityLabels as a data.table, so that we can look up the activityName by using the classLabel
+  activityLabels = data.table(activityLabels)
+  setkey(activityLabels, classLabel)
+  
+##Read Data 
 #Create a features vector
 feat <- read.table(".\\features.txt")
-feat1 <- as.vector(feat[,2])
+features <- as.vector(feat[,2])
 
-#Merge training set and data label
-dat1 <- read.table(".\\train\\X_train.txt")
-subj1 <- read.table(".\\train\\subject_train.txt")
-colnames(subj1) <- "Subject"
-colnames(dat1) <- feat1
-dtrain <- cbind(subj1,dat1)
+#Read activity labels
+ActivityLabels = read.table("./activity_labels.txt", stringsAsFactors = F)
+head(ActivityLabels)
+colnames(ActivityLabels) = c("Label","ActivityName")
 
-#Merge test set and data label
-dat2 <- read.table(".\\test\\X_test.txt")
-subj2 <- read.table(".\\test\\subject_test.txt")
-colnames(subj2) <- "Subject"
-colnames(dat2) <- feat1
-dtest <- cbind(subj2, dat2)
+#Read training data and create a dataset including subjects and activities information
+datTrain <- read.table(".\\train\\X_train.txt")
+activitiesTrain <- read.table(".\\train\\Y_train.txt")
+subjTrain <- read.table(".\\train\\subject_train.txt")
+datasetTrain <- cbind(subjTrain, activitiesTrain, datTrain)
 
-#Merge the training and the test sets
-dat2 <- rbind(dtrain, dtest)
+#Read test data and create a dataset including subjects and activities information
+datTest <- read.table(".\\test\\X_test.txt")
+activitiesTest <- read.table(".\\test\\Y_test.txt")
+subjTest <- read.table(".\\test\\subject_test.txt")
+datasetTest <- cbind(subjTest, activitiesTest, datTest)
 
-#Extracting the mean and standard deviation for each measurement
-meandat <- grep("mean", feat1)
-stddat  <- grep("std", feat1)
-tardat <- sort(c(meandat,stddat))
-dat3_tidy <- dat2[,tardat]
-head(dat3_tidy)
+##Point 1: Merges the training and the test sets to create one data set.
+Dataset<- rbind(datasetTrain, datasetTest)
+  
+  #Appropriately labels the data set with descriptive variable names (Point 4)
+  colnames(Dataset) <- c("Subject", "Activity", as.character(features))
+
+##Point 2: Extracts only the measurements on the mean and standard deviation for each measurement
+  
+  #Get the corresponding columns index
+  mean_std_columns = grep("(mean|std)[^meanFreq]", colnames(Dataset))
+
+  #Build dataset with corresponding columns, and keeping the subject column.
+  Dataset_mean_and_std <- Dataset[,c(1,2,mean_std_columns)]
+
+##Point 3: Uses descriptive activity names to name the activities in the data set
+  
+for (i in 1:length(Dataset_mean_and_std$Activity)){
+  if ((Dataset_mean_and_std[i,2] == 1)==TRUE){
+    Dataset_mean_and_std[i,2] <- "WALKING"
+  } 
+  else if ((Dataset_mean_and_std[i,2] == 2)==TRUE){
+    Dataset_mean_and_std[i,2] <- "WALKING_UPSTAIRS"
+  }
+  else if ((Dataset_mean_and_std[i,2] == 3)==TRUE){
+    Dataset_mean_and_std[i,2] <- "WALKING_DOWNSTAIRS"
+  }
+  else if ((Dataset_mean_and_std[i,2] == 4)==TRUE){
+    Dataset_mean_and_std[i,2] <- "SITTING"
+  }
+  else if ((Dataset_mean_and_std[i,2] == 5)==TRUE){
+    Dataset_mean_and_std[i,2] <- "STANDING"
+  }
+  else if ((Dataset_mean_and_std[i,2] == 6)==TRUE){
+    Dataset_mean_and_std[i,2] <- "LAYING"
+  }
+}
+
+##Point 5: creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+library(data.table)
+Dataset_mean_and_std_dt = data.table(Dataset_mean_and_std)
+setkey(Dataset_mean_and_std_dt,Activity, Subject)
+Tidy_data_set = Dataset_mean_and_std_dt[,lapply(.SD,mean), by = .(Activity,Subject)]
+write.table(Tidy_data_set, "By_Activity_and_Subject_average.txt", row.name = F)
+
 }
